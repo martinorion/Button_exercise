c7f47d08addc8ca8e0e13038238cd6c3741fc32d
Kristián Jakubik <martinkiko18@gmail.com>
2021-02-19 11:22:20 +0100

    Add project files.

---
+++ Button_exercise/Button_exercise/Page1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Button_exercise
+{
+    public class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            var nextPageButton = new Button { Text = "Super", VerticalOptions = LayoutOptions.CenterAndExpand };
+            nextPageButton.Clicked += OnNextPageButtonClicked;
+
+            var nextPageButtonA = new Button { Text = "nie tak super", VerticalOptions = LayoutOptions.CenterAndExpand };
+            nextPageButtonA.Clicked += OnNextPageButtonClickedA;
+
+            var nextPageButtonB = new Button { Text = "najlepšie", VerticalOptions = LayoutOptions.CenterAndExpand };
+            nextPageButtonB.Clicked += OnNextPageButtonClickedB;
+
+			Title = "Príznaky";
+			Content = new StackLayout
+			{
+				Children = {
+					nextPageButton,
+					nextPageButtonA,
+					nextPageButtonB,
+				
+				}
+			};
+		}
+		
+		async void OnNextPageButtonClicked(object sender, EventArgs e)
+			{
+				await Navigation.PushAsync(new Page2Xaml());
+			}
+			async void OnNextPageButtonClickedA(object sender, EventArgs e)
+			{
+				await Navigation.PushAsync(new Page2Xaml());
+			}
+			async void OnNextPageButtonClickedB(object sender, EventArgs e)
+			{
+				await Navigation.PushAsync(new Page2Xaml());
+			}
+		}
+    }
---
+++ Button_exercise/Button_exercise/Page1Xaml.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Button_exercise
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1Xaml : ContentPage
+    {
+        public Page1Xaml()
+        {
+            InitializeComponent();
+        }
+       
+
+        async void OnNextPageButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page2Xaml());
+        }
+        async void OnNextPageButtonClickedA(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page2Xaml());
+        }
+        async void OnNextPageButtonClickedB(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page2Xaml());
+        }
+    }
+}
